ea409f2a05f904f29627d4e5ae6b96ade22298da
vadim491 <69072790+vadim491@users.noreply.github.com>
2025-09-20 20:24:01 +0300

    Update a.cmd

--- a.cmd
+++ a.cmd
@@ -1,1 +1,1 @@
-powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('I love you.', 'like', 'OK', 'Information')"
+powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Any code here.', 'By odamvpti', 'OK', 'Information')"
