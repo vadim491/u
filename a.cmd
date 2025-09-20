ea5d4f64e39009a6996b59c224a9ddcd7d2bfabd
vadim491 <69072790+vadim491@users.noreply.github.com>
2025-09-20 20:29:40 +0300

    Update a.cmd

--- a.cmd
+++ a.cmd
@@ -1,1 +1,1 @@
-powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Any code here.', 'By odamvpti', 'OK', 'Information')"
+powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Any code not here.', 'By odamvpti', 'OK', 'Information')"
